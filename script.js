9d33490f121011dcd9ee2c5e2eefe3cbc04dd57f
Hardik Hadiya <hardikhadiya21@gmail.com>
2025-07-15 20:33:28 +0530

    1.0

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let boxes = document.querySelectorAll(".box");
 
-let reset_btn = document.getElementsByClassName("reset");
+let reset_btn = document.querySelector(".reset");
+let win = document.querySelector(".win-player");
+let msg = document.querySelector("#msg");
+let new_game = document.querySelector("#new-game");
+
 
 let turnO = true;
 
@@ -14,6 +18,12 @@ let win_pattern = [
     [3,4,5],
     [6,7,8],
 ];
+
+const resetGame = () => {
+    turnO = false;
+    enabled();
+    win.classList.add("hide");
+}
 
 boxes.forEach((box) => {
     box.addEventListener("click",() =>{
@@ -30,6 +40,38 @@ boxes.forEach((box) => {
     }); 
 });
 
-const checkWinner = () => {
-    
+const disabled = () => {
+    for(box of boxes){
+        box.disabled = true;
+    }
 }
+
+const enabled = () => {
+    for(box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const winner = (winner) => {
+    msg.innerText = `Congratulations Winner is ${winner}`;
+    win.classList.remove("hide");
+    disabled();
+}
+
+const checkWinner = () => {
+    for(let patterns of win_pattern){
+        let val1 = boxes[patterns[0]].innerText;
+        let val2 = boxes[patterns[1]].innerText;
+        let val3 = boxes[patterns[2]].innerText;
+
+        if(val1 != "" && val2 != "" && val3 != ""){
+            if(val1 === val2 && val2 === val3){
+                winner(val1);
+            }
+        }
+    }
+}
+
+new_game.addEventListener("click",resetGame);
+reset_btn.addEventListener("click",resetGame);
